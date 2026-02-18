42dc85f030b5c5c2fb958455bd6f850de0863b00
a.odegov <a.odegov@oos.ru>
2026-02-18 18:08:06 +0300

    v1.0.0

---
+++ service_locator/local.apps.service.php
@@ -0,0 +1,10 @@
+<?php
+
+defined('B_PROLOG_INCLUDED') || die;
+
+return [
+    'base.module.local.apps.service' => [
+        'className' => Base\Module\Src\LocalApps\LocalAppsService::class,
+        'constructorParams' => [],
+    ],
+];
